ca9de5af6e010bd5cc2e26c76541b17a7005f92d
Miguel Parra <miguelparradev@gmail.com>
2021-07-21 14:44:00 -0600

    style(Menu.tsx): Intros added to separate functions and imports

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { State } from "../../store";
-
 import "./Menu.css";
+
 const Menu = (): JSX.Element => {
   const { visible } = useSelector((state: State) => state.menu);
   const [classes, setClasses] = useState<string>("Menu-Close");
+
   const openMenu = () => {
     setClasses("Menu-Transparent");
     setTimeout(() => {
       setClasses("");
     }, 200);
   };
+
   const closeMenu = () => {
     setClasses("Menu-Transparent");
     setTimeout(() => {
       setClasses("Menu-Close");
     }, 200);
   };
+
   useEffect(() => {
     visible ? openMenu() : closeMenu();
   }, [visible]);
+
   return (
     <div className={`Menu translucentBackground transition-basic ${classes}`}>
       Hola mundo{" "}
