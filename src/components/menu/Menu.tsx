8b3dc0ee3d74ee46cd796029f5f4fa5eea0a5290
Miguel Parra <miguelparradev@gmail.com>
2021-07-21 14:40:30 -0600

    feat: Menu transition efect on open and close

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { State } from "../../store";
 
 import "./Menu.css";
 const Menu = (): JSX.Element => {
   const { visible } = useSelector((state: State) => state.menu);
-	const dynamicStyle:React.CSSProperties = {
-		display: visible ? 'block':'none',
-	};
-  return (<div className="Menu translucentBackground" style={dynamicStyle}>Hola mundo </div>);
+  const [classes, setClasses] = useState<string>("Menu-Close");
+  const openMenu = () => {
+    setClasses("Menu-Transparent");
+    setTimeout(() => {
+      setClasses("");
+    }, 200);
+  };
+  const closeMenu = () => {
+    setClasses("Menu-Transparent");
+    setTimeout(() => {
+      setClasses("Menu-Close");
+    }, 200);
+  };
+  useEffect(() => {
+    visible ? openMenu() : closeMenu();
+  }, [visible]);
+  return (
+    <div className={`Menu translucentBackground transition-basic ${classes}`}>
+      Hola mundo{" "}
+    </div>
+  );
 };
 export default Menu;
