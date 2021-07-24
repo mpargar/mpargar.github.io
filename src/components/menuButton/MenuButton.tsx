891127c7b1516af456d383aa4f14d62f6c53cf50
Miguel Parra <miguelparradev@gmail.com>
2021-07-24 16:47:14 -0600

    refactor(MenuButton.tsx): Unnecessary code removed

--- src/components/menuButton/MenuButton.tsx
+++ src/components/menuButton/MenuButton.tsx
@@ -1,9 +1,8 @@
 import Icon from "../icon/Icon";
 import MenuIcon from "../../assets/img/Menu.svg";
 import { bindActionCreators } from "redux";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import * as MenuActionCreators from "../../store/reducers/menu/action-creators";
-import { State } from "../../store";
 
 const MenuButton = (): JSX.Element => {
   const dispatch = useDispatch();
